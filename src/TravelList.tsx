9e1a02edff87a96ab767e3943be12b1e850e4722
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-08 23:17:42 +0900

    Using Recoil
    Recoil을 이용해서 state를 분리시키도록 하고, Country가 어떻게 생겼는지 알려줄 인터페이스를 만들었다. 추가로, 그동안 입력받은 것과 새로 입력받을 Country를 추가하도록 함수를 지정한 후에 입력된 값을 리스트로 출력하도록 했다.

--- src/TravelList.tsx
+++ src/TravelList.tsx
@@ -1,14 +1,34 @@
 import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
 
-interface ICountry {
+const travelState = atom<ICountry[]>({
+    key:"country",
+    default: [],
+})
+
+interface IForm {
     Country:string,
 }
 
+interface ICountry {
+    id:number;
+    text:string;
+    category: "Country" | "Traveled" | "Favorite";
+
+}
+
 function TravelList() {
-    const { register, handleSubmit, formState: {errors}, setError, setValue} = useForm<ICountry>();
-    const onValid = () => {
+    const [countryState, setCountryState] = useRecoilState(travelState);  //  이 함수는 value와 modifier을 반환한다.
+    /*
+    const value = useRecoilValue(travelState);  //  1. atom으로부터 값을 불러옴, value는 array
+    const modFn = useSetRecoilState(travelState);  //  2. atom의 값을 바꾸는 modifier funciton
+    */
+    const { register, handleSubmit, formState: {errors}, setError, setValue} = useForm<IForm>();
+    const onValid = ({Country}:IForm) => {
+        setCountryState((prev) => [{id:Date.now(), text:Country, category:"Country"}, ...prev]);
         setValue("Country", "");
-    }
+    };
+    console.log(countryState);
     return (
         <div>
             <h1>내가 가고 싶은 나라들</h1>
@@ -24,6 +44,9 @@ function TravelList() {
                 <span>{errors?.Country?.message}</span>
                 <button>가즈아!!!</button>
             </form>
+            <ul>
+                {countryState.map((country) => <li key={country.id}>{country.text}</li>)}
+            </ul>
         </div>
         </div>
     );
