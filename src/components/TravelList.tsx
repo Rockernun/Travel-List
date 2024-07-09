01c604eb3c0ad714e301869e1fb6bb142a77d599
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-07-09 21:02:56 +0900

    Category Rendering
    Recoil을 사용하여 세 가지 카테고리의 나라 목록을 관리.
    
    ("가고 싶은 나라들", "가본 나라들", "좋아하는 나라들") 각 카테고리는 CountrySelector를 통해 파생된 상태를 사용하여 나타내며, 사용자는 나라를 생성하고 각 나라를 클릭하여 상태를 변경할 수 있게 했다.

--- src/components/TravelList.tsx
+++ src/components/TravelList.tsx
@@ -1,18 +1,25 @@
 import { useRecoilValue } from "recoil";
-import { travelState } from "../atoms";
+import { CountrySelector, travelState } from "../atoms";
 import CreateCountry from "./CreateCountry";
 import Country from "./Country";
 
 function TravelList() {
-    const countryState = useRecoilValue(travelState);
-    console.log(countryState);
+    const [country, traveled, favorite] = useRecoilValue(CountrySelector);
     return (
         <div>
             <h1>내가 가고 싶은 나라들</h1>
             <CreateCountry />
         <div>
             <ul>
-                {countryState.map((country) => <Country key={country.id} {...country} />)} 
+                {country && country.map((country) => <Country key={country.id} {...country} />)} 
+            </ul>
+            <h1>내가 가본 나라들</h1>
+            <ul>
+                {traveled && traveled.map((country) => <Country key={country.id} {...country} />)}   
+            </ul>
+            <h1>내가 좋아하는 나라들</h1>
+            <ul>
+                {favorite && favorite.map((country) => <Country key={country.id} {...country} />)}   
             </ul>
         </div>
         </div>
